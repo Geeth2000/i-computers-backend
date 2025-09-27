858ec80ecb80fa889e64180bcadfa34a34e2c9ba
Geethkaru <geethanjankaru1232gmail.com>
2025-09-27 14:22:54 +0530

    Using async-await

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,80 +1,8 @@
-// import Product from "../models/Product.js";
-// import { isAdmin } from "./userController.js";
-
-// export function createProduct(req, res) {
-//   if (!isAdmin(req)) {
-//     res.status(403).json({
-//       message: "Forbidden",
-//     });
-//     return;
-//   }
-
-//   const product = new Product(req.body);
-
-//   product
-//     .save()
-//     .then(() => {
-//       res.json({
-//         message: "Product created successfully",
-//       });
-//     })
-//     .catch((error) => {
-//       res.status(500).json({
-//         message: "Error creating product",
-//         error: error.message,
-//       });
-//     });
-// }
-
-// export function getAllProducts(req, res) {
-//   if (isAdmin(req)) {
-//     Product.find()
-//       .then((products) => {
-//         res.json(products);
-//       })
-//       .catch((error) => {
-//         res.status(500).json({
-//           message: "Error fetching products",
-//           error: error.message,
-//         });
-//       });
-//   } else {
-//     Product.find({ isAvailable: true })
-//       .then((products) => {
-//         res.json(products);
-//       })
-//       .catch((error) => {
-//         res.status(500).json({
-//           message: "Error fetching products",
-//           error: error.message,
-//         });
-//       });
-//   }
-// }
-
-// export function deleteProduct(req, res) {
-//   if (!isAdmin(req)) {
-//     res.status(403).json({
-//       message: "Only admin can delete products",
-//     });
-//     return;
-//   }
-
-//   const productID = req.params.productID;
-
-//   Product.deleteOne({ productID: productID }).then(() => {
-//     res.json({
-//       message: "Product deleted successfully",
-//     });
-//   });
-// }
-
 import Product from "../models/Product.js";
 import { isAdmin } from "./userController.js";
 
 export function createProduct(req, res) {
   if (!isAdmin(req)) {
-    // Only check admin status
     res.status(403).json({
       message: "Forbidden",
     });
@@ -98,37 +26,45 @@ export function createProduct(req, res) {
     });
 }
 
-export function getAllProducts(req, res) {
-  if (isAdmin(req)) {
-    // If admin, show all products
-    Product.find()
-      .then((products) => {
-        res.json(products);
-      })
-      .catch((error) => {
-        res.status(500).json({
-          message: "Error fetching products",
-          error: error.message,
+export async function getAllProducts(req, res) {
+  try {
+    if (isAdmin(req)) {
+      // Product.find()
+      // 	.then((products) => {
+      // 		res.json(products);
+      // 	})
+      // 	.catch((error) => {
+      // 		res.status(500).json({
+      // 			message: "Error fetching products",
+      // 			error: error.message,
+      // 		});
+      // 	});
+
+      // Using async-await
+
+      const products = await Product.find();
+    } else {
+      Product.find({ isAvailable: true })
+        .then((products) => {
+          res.json(products);
+        })
+        .catch((error) => {
+          res.status(500).json({
+            message: "Error fetching products",
+            error: error.message,
+          });
         });
-      });
-  } else {
-    // If not admin, show only available products
-    Product.find({ isAvailable: true })
-      .then((products) => {
-        res.json(products);
-      })
-      .catch((error) => {
-        res.status(500).json({
-          message: "Error fetching products",
-          error: error.message,
-        });
-      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Error fetching products",
+      error: error,
+    });
   }
 }
 
 export function deleteProduct(req, res) {
   if (!isAdmin(req)) {
-    // Check if admin
     res.status(403).json({
       message: "Only admin can delete products",
     });
@@ -191,3 +127,4 @@ export function getProductByID(req, res) {
       });
     });
 }
+// add try catch for async-await
